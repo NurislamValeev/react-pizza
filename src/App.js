75300753cf5332c7a60cec6acfe4f1757ecf48f8
Nurislam <nurislamvaleev@gmail.com>
2021-07-28 02:10:05 +0300

    added PizzaBlock.jsx and db.json

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {Route} from 'react-router-dom'
 
 import {Header} from "./components"
-import {Home, Cart} from "./pages"
+import {Cart, Home} from "./pages"
 
-const App = () => (
-  <div className="wrapper">
-    <Header/>
-    <div className="content">
-      <Route exact path='/' component={Home}/>
-      <Route exact path='/cart' component={Cart}/>
+const App = () => {
+
+  const [pizzas, setPizzas] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:3000/db.json')
+      .then((res) => res.json())
+      .then((json) => {
+        setPizzas(json.pizzas)
+      })
+  }, [])
+
+  return (
+    <div className="wrapper">
+      <Header/>
+      <div className="content">
+        <Route exact path='/' render={() => <Home pizzas={pizzas}/>}/>
+        <Route exact path='/cart' component={Cart}/>
+      </div>
     </div>
-  </div>
-);
+  )
+}
+
 
 export default App
--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,12 @@ import Header from "./Header/Header"
 import Button from "./Button/Button"
 import Categories from "./Categories/Categories"
 import SortPopup from "./SortPopup/SortPopup"
+import PizzaBlock from "./PizzaBlock/PizzaBlock"
 
 export {
   Header,
   Button,
   Categories,
-  SortPopup
+  SortPopup,
+  PizzaBlock
 }
