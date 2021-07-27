c5bb9722c2c10592dc1d622f9aafeb4da3910c61
Nurislam <nurislamvaleev@gmail.com>
2021-07-27 21:43:40 +0300

    added Cart, Home and SortPopup components, react-router

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import Header from "./Header/Header"
 import Button from "./Button/Button"
 import Categories from "./Categories/Categories"
+import SortPopup from "./SortPopup/SortPopup"
 
 export {
   Header,
   Button,
-  Categories
+  Categories,
+  SortPopup
 }
