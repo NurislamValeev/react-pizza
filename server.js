fc5160d42af3a748cc6aa45d6aa2a6a93ca12d5e
Nurislam <nurislamvaleev@gmail.com>
2021-08-25 13:37:15 +0300

    added 0.0.0.0

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ const PORT = process.env.port || 3001
 server.use(middlewares)
 server.use(router)
 
-server.listen(PORT, () => {
+server.listen(PORT, "0.0.0.0", () => {
   console.log('Server is running on port ' + PORT)
 })
